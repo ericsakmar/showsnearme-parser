d64a5b020b9994d6b9cf672825dc796d5b0448a2
Eric Sakmar <eric.sakmar@gmail.com>
2016-06-04 13:42:25 -0400

    adds api and connection info

--- api/index.js
+++ api/index.js
@@ -1,13 +1,25 @@
-var express = require('express'),
+const express = require('express'),
     router = express.Router(),
-    Message = require('../models/message');
+    config = require('../config'),
+    FB = require('fb');
 
 router.get('/:id', function(req, res, next) {
-  const e = {
-    msg: 'ok'
+
+  const options = {
+    appId: config.fb.app_id,
+    xfbml: true,
+    version: 'v2.6'
   };
 
-  res.json(e);
+  const fb = new FB.Facebook(options);
+
+  const params = {
+    access_token: config.fb.access_token 
+  };
+
+  fb.api(`/${req.params.id}`, params, (e) => {
+    res.json(e);
+  });
 });
 
 module.exports = router;
