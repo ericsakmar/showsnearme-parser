44b1279509481969ed13ae372463311d0b6b461b
Eric Sakmar <eric.sakmar@gmail.com>
2016-08-26 22:50:23 -0400

    parses links
    uses link parser

--- test/parser.js
+++ test/parser.js
@@ -1,6 +1,40 @@
-const should = require('should');
+const should = require('should'),
+  parser = require('../api/parser');
 
 describe('parser', () => {
-  it('parses', () => {
+
+  describe('parses urls', () => {
+
+    it('one', () => {
+      const expected = ['https://showponiesindc.bandcamp.com/'];
+      const text = 'foo show blah blah https://showponiesindc.bandcamp.com/ other stuff';
+      const actual = parser.findLinks(text);
+      actual.should.deepEqual(expected);
+    });
+
+    it('more than one', () => {
+      const expected = ['https://showponiesindc.bandcamp.com/', 'https://swimsuitaddition.bandcamp.com/'];
+      const text = 'foo show blah blah https://showponiesindc.bandcamp.com/ other https://swimsuitaddition.bandcamp.com/ stuff';
+      const actual = parser.findLinks(text);
+      actual.should.deepEqual(expected);
+    });
+    
+    it('none', () => {
+      const expected = [];
+      const text = 'foo show blah blah other stuff';
+      const actual = parser.findLinks(text);
+      actual.should.deepEqual(expected);
+    });
+
+    it('long link', () => {
+      const expected = ['https://showponiesindc.bandcamp.com/album/slow-danger'];
+      const text = 'foo show blah blah https://showponiesindc.bandcamp.com/album/slow-danger    other stuff';
+      const actual = parser.findLinks(text);
+      actual.should.deepEqual(expected);
+    });
+
+
   });
+    
+    
 });
